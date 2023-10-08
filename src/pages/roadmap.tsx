836d469f7034e228fb6eb7703e22b9df3b018828
WEBzaytsev <studio@webzaytsev.ru>
2023-10-08 21:30:31 +0300

    refactor(Roadmap page): remake for using data from DB

--- src/pages/roadmap.tsx
+++ src/pages/roadmap.tsx
@@ -1,14 +1,50 @@
 import Layout from "../layout";
 import roadMap from '@/assets/roadmap.png';
+import {useLayoutEffect, useState} from "react";
+import edjsHTML from 'editorjs-html';
+
+const edjsParser = edjsHTML();
 
 const RoadMapPage = () => {
+    const [content, setContent] = useState<any[]>();
+    const [title, setTitle] = useState('');
+    const [loading, setLoading] = useState(true);
+    const postId = 4;
+
+    useLayoutEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`http://localhost:3000/api/v1/posts/${postId}`, {
+                method: "GET",
+                headers: {
+                    "access-control-allow-origin" : "*",
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            });
+            const newData = await response.json();
+            setTitle(newData.title);
+            const html = edjsParser.parse(newData.content);
+            setContent(html);
+            setLoading(false);
+        };
+        fetchData();
+    }, [])
+
+    if(loading) {
+        return (
+            <Layout>
+                Загрузка...
+            </Layout>
+        )
+    }
+
+    const formattedHtml = () => { return {__html:  content?.map((item: any) => {return item}).join("")}; };
+
     return (
         <Layout>
             <div className="content-text">
-                <div className="title">Куда идти стартапам в разных странах?</div>
+                <div className="title">{title}</div>
+                <div className="paragraph" dangerouslySetInnerHTML={formattedHtml() as unknown as { __html: string }} />
                 <div className="paragraph">
-                    <p>Каждый месяц редакция RB.RU рассказывает, какие возможности разные страны предлагают IT-компаниям. Среди них: бизнес-инкубаторы, акселераторы, стартап-школы, фонды, мероприятия, стартап-визы и многое другое.</p>
-                    <p>Параллельно мы разбираем налоговые системы выбранных стран, чтобы предприниматели могли объективнее оценить потенциал своего бизнеса в новой локации.</p>
                     <a href="#" className="btn">Перейти к проекту</a>
                 </div>
             </div>
