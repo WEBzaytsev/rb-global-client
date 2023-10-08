da792fb3a886f5f777df8ae4fce105c86c65f176
WEBZaitsev <studio@webzaytsev.ru>
2023-10-08 23:10:17 +0300

    revert: :fire: Update the settings to be more semantic

--- src/utils/getApiUrl.ts
+++ src/utils/getApiUrl.ts
@@ -1,6 +1,6 @@
 export const getPostDataById = (postId: string | number): string => {
-    const host = import.meta.env.API_HOST;
-    const protocol = import.meta.env.API_PORT;
+    const host = import.meta.env.VITE_HOST;
+    const protocol = import.meta.env.VITE_PORT;
     const path = 'api/v1/posts';
 
     return `${host}:${protocol}/${path}/${postId}`;
