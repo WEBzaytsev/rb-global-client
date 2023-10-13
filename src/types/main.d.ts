0e42b5fae5eacc657e9c792dc90132475b0e615e
WEBzaytsev <studio@webzaytsev.ru>
2023-10-13 23:33:17 +0300

    refactor: define env variables

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,9 +1,14 @@
 declare module "@/*";
 declare module "dangerouslySetInnerHTML";
+
 interface ImportMetaEnv extends Readonly<Record<string, string>> {
-  readonly VITE_APP_TITLE: string;
+    readonly VITE_API_PROTOCOL: string;
+    readonly VITE_API_HOSTNAME: string;
+    readonly VITE_API_PORT: string;
+    readonly VITE_API_PATHNAME: string;
+    readonly VITE_APP_TITLE: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+    readonly env: ImportMetaEnv;
 }
