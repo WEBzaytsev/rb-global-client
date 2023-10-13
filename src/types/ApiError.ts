07310c56990378f7212e5741705eeb200fe643b2
WEBzaytsev <studio@webzaytsev.ru>
2023-10-13 23:36:08 +0300

    refactor: add types

---
+++ src/types/ApiError.ts
@@ -0,0 +1,5 @@
+export interface ApiError {
+    status: boolean;
+    name: string;
+    message: string;
+}
---
+++ src/types/BlockTypes.ts
@@ -0,0 +1,8 @@
+export enum BlockTypes {
+    TEXT_CONTENT = 'textContent',
+    IMAGE = 'image',
+    BUTTON = 'button',
+    EVENTS = 'events',
+    TAGS = 'tags',
+    ARTICLES = 'articles',
+}
